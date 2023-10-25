b08d51d6364e6d3496d3b490a341866eaa7db8df
Monel Lindu <monel.redlind@gmail.com>
2023-10-25 14:56:19 +0700

    Working with child category CRUD and status toggle

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\ChildCategoryController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\SubCategoryController;
@@ -26,3 +27,8 @@ Route::resource('category', CategoryController::class);
 // sub categories routes
 Route::put('subcategory/change-status', [SubCategoryController::class, 'changeStatus'])->name('subcategory.change-status');
 Route::resource('subcategory', SubCategoryController::class);
+
+// child categories routes
+Route::put('child-category/change-status', [ChildCategoryController::class, 'changeStatus'])->name('child-category.change-status');
+Route::get('get-subcategories', [ChildCategoryController::class, 'getSubCategories'])->name('get-subcategories');
+Route::resource('child-category', ChildCategoryController::class);
