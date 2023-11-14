01e7b8753ea85a8d7cb5eaf5c1fe306951a8f0c4
Monel Lindu <monel.redlind@gmail.com>
2023-11-14 17:29:42 +0700

    making admin's sidebar active

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -29,12 +29,21 @@
         <li class="dropdown {{setActive([
           'admin.brand.*',
           'admin.products.*',
-          'admin.seller-product.*'
+          'admin.products-image-gallery.*',
+          'admin.products-variant.*',
+          'admin.products-variant-item.*',
+          'admin.seller-product.*',
+          'admin.seller-pending-products.*'
         ])}}">
           <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Manage Products</span></a>
           <ul class="dropdown-menu">
             <li class="{{setActive(['admin.brand.*'])}}"><a class="nav-link" href="{{route('admin.brand.index')}}">Brands</a></li>
-            <li class="{{setActive(['admin.products.*'])}}"><a class="nav-link" href="{{route('admin.products.index')}}">Product</a></li>
+            <li class="{{setActive([
+              'admin.products.*',
+              'admin.products-image-gallery.*',
+              'admin.products-variant.*',
+              'admin.products-variant-item.*',
+            ])}}"><a class="nav-link" href="{{route('admin.products.index')}}">Product</a></li>
             <li class="{{setActive(['admin.seller-product.*'])}}"><a class="nav-link" href="{{route('admin.seller-product.index')}}">Seller Product</a></li>
             <li class="{{setActive(['admin.seller-pending-products.*'])}}"><a class="nav-link" href="{{route('admin.seller-pending-products.index')}}">Seller Pending Products</a></li>
           </ul>
