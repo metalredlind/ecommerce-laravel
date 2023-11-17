67d92318f3a5ff363d620e157e19bea992f782a1
Monel Lindu <monel.redlind@gmail.com>
2023-11-17 17:02:37 +0700

    showing flash sale items in front end

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,7 +1,6 @@
 <?php
 
 /* Set Sidebar item active */
-
 function setActive(array $route)
 {
     if(is_array($route)){
@@ -10,5 +9,49 @@ function setActive(array $route)
                 return 'active';
             }
         }
+    }
+}
+
+/* check if product has discount */
+function checkDiscount($product)
+{
+    $currentDate = date('Y-m-d');
+
+    if($product->offer_price > 0 && $currentDate >= $product->offer_start_date && $currentDate <= $product->offer_end_date){
+        return true;
+    }
+    
+    return false;
+    
+}
+
+// calculate discount percent
+function calculateDiscountPercent($originalPrice, $discountPrice)
+{
+    $discountAmount = $originalPrice - $discountPrice;
+    $discountPercent = ($discountAmount/$originalPrice) * 100;
+
+    return $discountPercent;
+}
+
+//check the product type
+function productType($type)
+{
+    switch ($type) {
+        case 'new-arrival':
+            return "New";
+            break;
+        case 'featured_product':
+            return "Featured";
+            break;    
+        case 'top_product':
+            return "Top";
+            break;
+        case 'best_product':
+            return "Best";
+            break;
+        default:
+            return "";
+            break;
     }
 }
