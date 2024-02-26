d2d0385244a1cc7fd739256b98bd14eab82f80e4
Monel Lindu <monel.redlind@gmail.com>
2024-02-26 14:27:06 +0700

    cart - add to cart button in product homepage

--- resources/views/frontend/home/sections/flashsale.blade.php
+++ resources/views/frontend/home/sections/flashsale.blade.php
@@ -61,7 +61,20 @@
                             @else
                                 <p class="wsus__price">{{ $settings->currency_icon }}{{ $product->price }}</p>
                             @endif
-                            <a class="add_cart" href="#">add to cart</a>
+                            <form class="shopping-cart-form">
+                                <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                @foreach ($product->variants as $variant)
+                                    <select class="d-none" name="variants_items[]">
+                                        @foreach ($variant->productVariantItem as $variantItem)
+                                            <option value="{{ $variantItem->id }}"
+                                                {{ $variantItem->is_default == 1 ? 'selected' : '' }}>
+                                                {{ $variantItem->name }} (${{ $variantItem->price }})</option>
+                                        @endforeach
+                                    </select>
+                                @endforeach
+                                <input name="qty" type="hidden" min="1" max="100" value="1" />
+                                <button class="add_cart" href="#" type="submit">add to cart</button>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -155,32 +168,34 @@
                                                             @foreach ($variant->productVariantItem as $variantItem)
                                                                 <option value="{{ $variantItem->id }}"
                                                                     {{ $variantItem->is_default == 1 ? 'selected' : '' }}>
-                                                                    {{ $variantItem->name }} (${{ $variantItem->price }})</option>
+                                                                    {{ $variantItem->name }}
+                                                                    (${{ $variantItem->price }})</option>
                                                             @endforeach
                                                         </select>
                                                     </div>
                                                 @endforeach
                                             </div>
                                         </div>
-        
+
                                         <div class="wsus__quentity">
                                             <h5>quantity :</h5>
                                             <div class="select_number">
-                                                <input class="number_area" name="qty" type="text" min="1"
-                                                    max="100" value="1" />
+                                                <input class="number_area" name="qty" type="text"
+                                                    min="1" max="100" value="1" />
                                             </div>
                                         </div>
-        
+
                                         <ul class="wsus__button_area">
-                                            <li><button type="submit" class="add_cart" href="#">add to cart</button></li>
+                                            <li><button type="submit" class="add_cart" href="#">add to
+                                                    cart</button></li>
                                             <li><a class="buy_now" href="#">buy now</a></li>
                                             <li><a href="#"><i class="fal fa-heart"></i></a></li>
                                             <li><a href="#"><i class="far fa-random"></i></a></li>
                                         </ul>
                                     </form>
-                                    
+
                                     <p class="brand_model"><span>brand :</span> {{ $product->brand->name }}</p>
-                                    
+
                                 </div>
                             </div>
                         </div>
