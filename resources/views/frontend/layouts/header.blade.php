96cedbbca0d6ee0be1aacdf71d4b1184a88b3d42
Monel Lindu <monel.redlind@gmail.com>
2025-11-20 20:00:10 +0700

    null safe for wishlist count

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -34,7 +34,11 @@
                     </div>
                     <ul class="wsus__icon_area">
                         <li><a href="{{route('user.wishlist.index')}}"><i class="fal fa-heart"></i><span id="wishlist_count">
-                            {{ \App\Models\Wishlist::where('user_id', Auth::user()?->id)->count() }}
+                            @if (auth()->check())
+                                {{ \App\Models\Wishlist::where('user_id', Auth::user()->id)->count() }}
+                            @else
+                                0
+                            @endif
                         </span></a></li>
                         {{-- <li><a href="compare.html"><i class="fal fa-random"></i><span>03</span></a></li> --}}
                         <li><a class="wsus__cart_icon" href="#"><i class="fal fa-shopping-bag"></i><span
