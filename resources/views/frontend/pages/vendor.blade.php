3f4ce16f312b6b7b04e82809a2ec56f0329424a1
Monel Lindu <monel.redlind@gmail.com>
2026-03-23 21:53:24 +0700

    vendor pagination and debug bar disable

--- resources/views/frontend/pages/vendor.blade.php
+++ resources/views/frontend/pages/vendor.blade.php
@@ -59,24 +59,9 @@
                 </div>
                 <div class="col-xl-12">
                     <section id="pagination">
-                        <nav aria-label="Page navigation example">
-                            <ul class="pagination">
-                                <li class="page-item">
-                                    <a class="page-link" href="#" aria-label="Previous">
-                                        <i class="fas fa-chevron-left"></i>
-                                    </a>
-                                </li>
-                                <li class="page-item"><a class="page-link page_active" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#">4</a></li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#" aria-label="Next">
-                                        <i class="fas fa-chevron-right"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
+                        @if ($vendors->hasPages())
+                            {{ $vendors->links() }}
+                        @endif
                     </section>
                 </div>
             </div>
