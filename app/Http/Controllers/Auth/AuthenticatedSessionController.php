6bb44f5aa0cc94d72085e9443c85673957c182b1
Monel Lindu <monel.redlind@gmail.com>
2026-03-25 19:05:07 +0700

    show warning alert if inactive or banned

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,6 +29,15 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        if ($request->user()->status === 'inactive') {
+            Auth::guard('web')->logout();
+
+            $request->session()->regenerateToken();
+
+            toastr('Your account has been banned. Please contact customer suppport','error','Account Banned');
+            return redirect('/');
+        }
+
         if($request->user()->role == 'admin')
         {
             return redirect()->intended('/admin/dashboard');
